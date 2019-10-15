d250aee8cd2e91a1cead9b2ff43c36a613757018
Alex Lewitt <48691328+alewitt2@users.noreply.github.com>
2019-10-15 09:19:47 -0400

    Allow string templates (#26)
    * Allow string templates
    
    this will allow numbers and boolean to show up in final result
    
    * Update resource.yaml
    
    * Update MustacheTemplateController.js

--- src/MustacheTemplateController.js
+++ src/MustacheTemplateController.js
@@ -16,6 +16,7 @@
 
 const Mustache = require('mustache');
 const objectPath = require('object-path');
+const yaml = require('js-yaml');
 
 const { BaseTemplateController } = require('@razee/razeedeploy-core');
 
@@ -26,24 +27,42 @@ module.exports = class MustacheTemplateController extends BaseTemplateController
     super(params);
   }
 
-  async _stringify(o) {
-    return JSON.stringify(o);
+  async _stringifyTemplates(o) {
+    if (!Array.isArray(o)) {
+      return Promise.reject({ statusCode: 500, message: '_stringifyTemplates expects an array' });
+    }
+    o.forEach((obj, i) => {
+      if (typeof obj === 'object') {
+        o[i] = JSON.stringify(obj);
+      }
+    });
+    return o;
   }
 
-  async _parse(s) {
-    return JSON.parse(s);
+  async _parseTemplates(s) {
+    if (!Array.isArray(s)) {
+      return Promise.reject({ statusCode: 500, message: '_parseTemplates expects an array' });
+    }
+    s.forEach((str, i) => {
+      if (typeof str === 'string') {
+        s[i] = yaml.safeLoad(str);
+      }
+    });
+    return s;
   }
 
   async processTemplate(templates, view) {
     let customTags = objectPath.get(this.data, ['object', 'spec', 'custom-tags']);
-    let templatesString = await this._stringify(templates);
+    let templatesArr = await this._stringifyTemplates(templates);
     let tempTags = Mustache.tags;
     if (customTags) {
       Mustache.tags = customTags;
     }
-    let resultString = Mustache.render(templatesString, view);
+    templatesArr.forEach((templatesString, i) => {
+      templatesArr[i] = Mustache.render(templatesString, view);
+    });
     Mustache.tags = tempTags;
-    let result = await this._parse(resultString);
+    let result = await this._parseTemplates(templatesArr);
     return result;
   }
 
